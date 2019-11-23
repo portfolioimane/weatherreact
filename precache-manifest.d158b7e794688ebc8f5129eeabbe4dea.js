1d927b2e35126a184f1f7523161265b10356c58b
blogimane <blogimane.g@gmail.com>
2019-11-23 15:07:52 +0000

    Updates

---
+++ precache-manifest.d158b7e794688ebc8f5129eeabbe4dea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a7b916bbbba5ce0cc84f86d60c37e40",
+    "url": "/weatherreact/index.html"
+  },
+  {
+    "revision": "622fd83d6c40afceb033",
+    "url": "/weatherreact/static/css/main.761a0424.chunk.css"
+  },
+  {
+    "revision": "9ba977970ad4003d1468",
+    "url": "/weatherreact/static/js/2.2297ccef.chunk.js"
+  },
+  {
+    "revision": "622fd83d6c40afceb033",
+    "url": "/weatherreact/static/js/main.6bf04329.chunk.js"
+  },
+  {
+    "revision": "63a12a460a799965d837",
+    "url": "/weatherreact/static/js/runtime-main.2d158188.js"
+  }
+]);
